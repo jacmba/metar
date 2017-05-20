4f22458775c87cb96d9fd3283b8c32cf553c5420
Jacinto Mba Cantero <jacmba@gmail.com>
2017-05-20 15:11:18 +0100

    Reorganized code

--- src/index.js
+++ src/index.js
@@ -1,3 +1,9 @@
 ﻿'use strict';
 
-console.log('Metar!!');
+let Express = require('express');
+let app = Express();
+let port = process.env.METAR_PORT || 3000;
+
+app.listen(port, () => {
+  console.log(`Metar API listening on por ${port}`);
+});
