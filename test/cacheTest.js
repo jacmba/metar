85a0190e4b7aa2f4b15b47bb80db61c5a47dcf88
Jacinto Mba Cantero <jacmba@gmail.com>
2017-05-20 19:45:53 +0100

    Implemented web interface class

--- test/cacheTest.js
+++ test/cacheTest.js
@@ -9,7 +9,6 @@ This gun is advertised as the most popular gun in American crime.
 Do you believe that shit? It actually says that in the little book that comes
 with it: the most popular gun in American crime.
 Like they're actually proud of that shit.`;
-const NOW = new Date().getTime();
 
 let Cache = require('../src/lib/cache');
 let cache = new Cache(TESTDIR);
